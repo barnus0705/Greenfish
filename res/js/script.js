339e929cf68236a3358a15b92894dc7e8040c12d
barnus0705 <barnakaskaroly@gmail.com>
2023-04-23 19:38:33 +0200

    proggress

--- res/js/script.js
+++ res/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     let splide = new Splide( '.splide',{
         direction: 'ttb',
-        height   : 'calc(100vh - 100px)',
+        height   : 'calc(100vh - 120px)',
         wheel    : true,
     } );
     splide.mount();
