56f6415e7bf60fcd914aac01fa734cb8d02c25d2
barna <barnabaskaroly@gmail.com>
2023-04-25 17:31:54 +0200

    progress

--- res/js/script.js
+++ res/js/script.js
@@ -1,14 +1,9 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     let splide = new Splide( '.splide',{
         direction: 'ttb',
-        height   : 'calc(100vh - 120px)',
+        height   : 'calc(100vh - 64px)',
         wheel    : true,
     } );
     splide.mount();
 }
 );
-let link = document.getElementById("1");
-link.addEventListener("gonetohome", function (){
-   let splide = new Splide();
-   console.log(splide.getIndex().value)
-});
